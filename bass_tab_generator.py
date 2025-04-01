568e9fece2447e64de870d32ee634d2c38b50725
Hayden <haydenpc369@gmail.com>
2025-04-01 14:58:21 -0500

    tuesdays work

--- bass_tab_generator.py
+++ bass_tab_generator.py
@@ -2,22 +2,22 @@ import random
 
 key_maps ={
     "E Major":{
-        "E":[0,2,4],
-        "A":[0,2,4],
-        "D":[1,2,4],
-        "G":[1,3,4]
+        "E":[0,2,4,5,7,9,11,12],
+        "A":[2,7,9,11],
+        "D":[2,9],
+        "G":[4,9]
     },
     "A Major":{
-        "E":[0,1,4],
-        "A":[0,2,4],
-        "D":[1,4],
-        "G":[0,2,4]
+        "E":[5,7,9,10],
+        "A":[0,2,4,5,7,9,11],
+        "D":[7,9,11],
+        "G":[2,4,6,11]
     },
     "G Major":{
-        "E":[0,3],
-        "A":[0,2,5],
-        "D":[0,2,4],
-        "G":[0,2,4]
+        "E":[3,7,8,10],
+        "A":[2,3,5,10],
+        "D":[5,9,10],
+        "G":[0,4,5,7,11]
     }
 }
 
@@ -38,3 +38,25 @@ def make_note(previous_note=None, key_notes=None):
 
 def tab_bar(riff):
     tab = {"E":["-"] * 16,"A":["-"] * 16,"D":["-"] * 16,"G":["-"] * 16,}
+
+    for i, note in enumerate(riff):
+        if len(note) == 2:
+            string,fret = note
+            tab[string][i] = str(fret)
+    output = "\n".join(f"{s}|{''.join(f'{n}-' if n != '-' else '--' for n in notes)}|" for s, notes in tab.items())
+    return output
+
+def generate_riff():
+    key_notes = pick_key()
+    riff = []
+    previous_note = None
+
+    for _ in range(16):
+        if random.random() < 0.05:
+            note1 = make_note(previous_note, key_notes)
+            note2 = make_note(note1, key_notes)
+            riff.append(note1)
+            riff.append(note2)
+        else:
+            note = make_note(previous_note,key_notes)
+            riff.append(note)
